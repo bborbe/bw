26f8da1bfb6fe91284afb68a7efbe0f32100d947
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-26 12:47:59 +0100

    add os and release check

--- bundles/openvpn/items.py
+++ bundles/openvpn/items.py
@@ -1,3 +1,8 @@
+os = node.metadata.get('os', '')
+release = node.metadata.get('release', '')
+if os != 'ubuntu' or release != 'xenial':
+    raise Exception('{} {} is not supported by this bundle'.format(os, release))
+
 pkg_apt = {
     'openvpn': {},
 }
