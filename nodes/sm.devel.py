ecfca2dbea1b0774f83ad66976de45bf9ac91a2e
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-26 14:53:44 +0100

    add devel

---
+++ nodes/sm.devel.py
@@ -0,0 +1,9 @@
+nodes['sm.devel'] = {
+    'bundles': [
+    ],
+    'hostname': 'bborbe.devel.lf.seibert-media.net',
+    'metadata': {
+        'os': 'ubuntu',
+        'release': 'xenial',
+    },
+}
