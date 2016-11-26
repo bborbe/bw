c340c872f652eeca4f0e8ae6300e681096250ddc
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-26 14:09:42 +0100

    add monit checks

--- bundles/monit/items.py
+++ bundles/monit/items.py
@@ -6,3 +6,28 @@ if os != 'ubuntu' or release != 'xenial':
 pkg_apt = {
     'monit': {},
 }
+
+svc_systemd = {
+    'monit': {
+        'running': True,
+        'enabled': True,
+        'needs': ['pkg_apt:monit'],
+    },
+}
+
+files = {
+    '/etc/monit/conf.d/free_space.conf': {
+        'source': 'free_space.conf',
+        'content_type': 'mako',
+        'mode': '0644',
+        'owner': 'root',
+        'group': 'root',
+    },
+    '/etc/monit/conf.d/mailserver.conf': {
+        'source': 'mailserver.conf',
+        'content_type': 'mako',
+        'mode': '0644',
+        'owner': 'root',
+        'group': 'root',
+    },
+}
