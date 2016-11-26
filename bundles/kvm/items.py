8e6b34a95d3532e707c85b427673109adc48c138
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-26 12:47:43 +0100

    add kvm bundle

---
+++ bundles/kvm/items.py
@@ -0,0 +1,9 @@
+os = node.metadata.get('os', '')
+release = node.metadata.get('release', '')
+if os != 'ubuntu' or release != 'xenial':
+    raise Exception('{} {} is not supported by this bundle'.format(os, release))
+
+pkg_apt = {
+    'virt-top': {},
+    'virtinst': {},
+}
