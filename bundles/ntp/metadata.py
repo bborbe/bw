5043b7578ad9e295240b5bfa092b41d93a08a1db
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-04 15:48:16 +0100

    refactor

--- bundles/ntp/metadata.py
+++ bundles/ntp/metadata.py
@@ -1,6 +1,4 @@
 def ntp_cron(metadata):
     metadata.setdefault('cron', {}).setdefault('jobs', {})
-    metadata['cron']['jobs'] = {
-        'ntpdate': '15 * * * * root ntpdate -s de.pool.ntp.org > /dev/null',
-    }
+    metadata['cron']['jobs']['ntpdate'] = '15 * * * * root ntpdate -s de.pool.ntp.org > /dev/null'
     return metadata
