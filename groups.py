1af9c9e54a846a2523fd282a8aa018075574aa4b
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-13 13:10:05 +0100

    improve groups

--- groups.py
+++ groups.py
@@ -1,8 +1,11 @@
-from glob import glob
+from os import walk
 from os.path import join
 
 groups = {}
 
-for group in glob(join("groups", "*.py")):
-    with open(group, 'r') as f:
-        exec (f.read())
+for root, dirs, files in walk('groups'):
+    for filename in files:
+        if filename.endswith('.py'):
+            group = join(root, filename)
+            with open(group, 'r') as f:
+                exec (f.read())
