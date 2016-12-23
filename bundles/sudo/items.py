26699809d78b03df662018818fe25d0f447dc0c9
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-23 15:16:15 +0100

    keep home env

--- bundles/sudo/items.py
+++ bundles/sudo/items.py
@@ -3,19 +3,28 @@ release = node.metadata.get('release', '')
 if not (os == 'ubuntu' and release == 'xenial' or os == 'debian' and release == 'jessie'):
     raise Exception('{} {} is not supported by this bundle'.format(os, release))
 
-files = {
-    '/etc/sudoers.d/users': {
-        'source': 'users',
-        'content_type': 'mako',
-        'mode': '0440',
-        'owner': 'root',
-        'group': 'root',
-        'context': {
-            'users': dict((k, v) for k, v in node.metadata['users'].items() if v.get('sudo', False)),
-        },
+files = {}
+
+files['/etc/sudoers'] = {
+    'source': 'sudoers',
+    'content_type': 'text',
+    'mode': '0440',
+    'owner': 'root',
+    'group': 'root',
+    'context': {},
+}
+
+files['/etc/sudoers.d/users'] = {
+    'source': 'users',
+    'content_type': 'mako',
+    'mode': '0440',
+    'owner': 'root',
+    'group': 'root',
+    'context': {
+        'users': dict((k, v) for k, v in node.metadata['users'].items() if v.get('sudo', False)),
     },
 }
 
 pkg_apt = {
-    "sudo": {},
+    'sudo': {},
 }
