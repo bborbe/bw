a900a19fa99bef065dc5a3d0570cd805d695bad7
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-03 02:06:41 +0100

    add hosts

--- bundles/hostname/items.py
+++ bundles/hostname/items.py
@@ -3,19 +3,31 @@ release = node.metadata.get('release', '')
 if not (os == 'ubuntu' and release == 'xenial' or os == 'debian' and release == 'jessie'):
     raise Exception('{} {} is not supported by this bundle'.format(os, release))
 
-parts = node.hostname.split('.', 2)
-if len(parts) == 0:
-    raise Exception('invalid hostname {} is missing'.format(hostname))
+parts = node.hostname.split('.', 1)
+if len(parts) != 2:
+    raise Exception('invalid hostname {}'.format(node.hostname))
 
-files = {
-    '/etc/hostname': {
-        'source': 'hostname',
-        'content_type': 'mako',
-        'mode': '0644',
-        'owner': 'root',
-        'group': 'root',
-        'context': {
-            'hostname': parts[0],
-        },
+files = {}
+
+files['/etc/hostname'] = {
+    'source': 'hostname',
+    'content_type': 'mako',
+    'mode': '0644',
+    'owner': 'root',
+    'group': 'root',
+    'context': {
+        'hostname': parts[0],
+    },
+}
+
+files['/etc/hosts'] = {
+    'source': 'hosts',
+    'content_type': 'mako',
+    'mode': '0644',
+    'owner': 'root',
+    'group': 'root',
+    'context': {
+        'hostname': parts[0],
+        'fqdn': node.hostname,
     },
 }
