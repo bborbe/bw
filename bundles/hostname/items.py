a00e70a29f87d1e906e34aa618e74accf6e677d7
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-04 19:23:35 +0100

    set hostname

--- bundles/hostname/items.py
+++ bundles/hostname/items.py
@@ -3,11 +3,21 @@ release = node.metadata.get('release', '')
 if not (os == 'ubuntu' and release == 'xenial' or os == 'debian' and release == 'jessie'):
     raise Exception('{} {} is not supported by this bundle'.format(os, release))
 
+files = {}
+
+actions = {}
+
 parts = node.hostname.split('.', 1)
 if len(parts) != 2:
     raise Exception('invalid hostname {}'.format(node.hostname))
 
-files = {}
+name = parts[0]
+
+actions['set_hostname_{}'.format(name)] = {
+    'command': 'hostnamectl set-hostname {}'.format(name),
+    'triggered': True,
+    'cascade_skip': False,
+}
 
 files['/etc/hostname'] = {
     'source': 'hostname',
@@ -16,13 +26,14 @@ files['/etc/hostname'] = {
     'owner': 'root',
     'group': 'root',
     'context': {
-        'hostname': parts[0],
+        'hostname': name,
     },
+    'triggers': ['action:set_hostname_{}'.format(name)],
 }
 
 hosts_v4 = node.metadata.get('hosts', {}).get('ipv4', {})
 if len(hosts_v4) == 0:
-    hosts_v4['127.0.0.1'] = [node.hostname, parts[0]]
+    hosts_v4['127.0.0.1'] = [node.hostname, name]
 hosts_v6 = node.metadata.get('hosts', {}).get('ipv6', {})
 
 files['/etc/hosts'] = {
