7fb4aeb39f03c7da2071b2d609721c388f0bcacb
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-10 22:56:39 +0100

    refactor

--- bundles/authorized_key/items.py
+++ bundles/authorized_key/items.py
@@ -2,8 +2,8 @@ if not (node.os == 'ubuntu' and node.os_version == (16, 4) or node.os == 'debian
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
 for username, data in node.metadata['users'].items():
-    keys = data.get('authorized_keys')
-    if keys:
+    keys = data.get('authorized_keys', {})
+    if len(keys) > 0:
         homedir = data.get('home', '/home/{}'.format(username))
         directories = {
             '{}/.ssh'.format(homedir): {
