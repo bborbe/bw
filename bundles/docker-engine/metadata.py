e42a5a70f79d278dd67890d75bb1b25988d8d138
Benjamin Borbe <bborbe@nova.fritz.box>
2017-01-11 23:13:30 +0100

    fmt

--- bundles/docker-engine/metadata.py
+++ bundles/docker-engine/metadata.py
@@ -1,12 +1,12 @@
 def docker_repo(metadata):
     if metadata.get('docker', {}).get('enabled', False):
-    release = metadata.get('release', '')
-    metadata.setdefault('apt', {}).setdefault('repos', {})
-    metadata['apt']['repos']['docker'] = {
-        'gpg_key': '2C52609D',
-        'sources': ['deb https://apt.dockerproject.org/repo {os}-{release} main'.format(os=metadata.get('os'), release=release)],
-        'installed': metadata.get('docker', False),
-    }
+        release = metadata.get('release', '')
+        metadata.setdefault('apt', {}).setdefault('repos', {})
+        metadata['apt']['repos']['docker'] = {
+            'gpg_key': '2C52609D',
+            'sources': ['deb https://apt.dockerproject.org/repo {os}-{release} main'.format(os=metadata.get('os'), release=release)],
+            'installed': metadata.get('docker', False),
+        }
     return metadata
 
 
