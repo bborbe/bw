8f3f7a4b92322bfb1ac3abd8efbfabf9c5bb377c
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-16 21:19:06 +0100

    update kvm

--- bundles/kvm/items.py
+++ bundles/kvm/items.py
@@ -1,20 +1,40 @@
 if not (node.os == 'ubuntu' and node.os_version == (16, 4) or node.os == 'debian' and node.os_version == (8, 0)):
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
-pkg_apt = {
-    'qemu-kvm': {
+pkg_apt = {}
+svc_systemd = {}
+
+kvm_packages = (
+    'libvirt-bin',
+    'virt-top',
+    'virtinst',
+    'qemu-utils',
+    'qemu-kvm',
+)
+
+kvm_gui_packages = (
+    'virt-manager',
+    'spice-client-gtk',
+)
+
+for pkg in kvm_packages:
+    pkg_apt[pkg] = {
         'installed': node.metadata.get('kvm', {}).get('enabled', False),
-    },
-    'virt-top': {
-        'installed': node.metadata.get('kvm', {}).get('enabled', False),
-    },
-    'virtinst': {
-        'installed': node.metadata.get('kvm', {}).get('enabled', False),
-    },
-    'virt-manager': {
+    }
+
+for pkg in kvm_gui_packages:
+    pkg_apt[pkg] = {
         'installed': node.metadata.get('kvm', {}).get('enabled', False) and node.metadata.get('kvm', {}).get('gui', False),
-    },
-    'spice-client-gtk': {
-        'installed': node.metadata.get('kvm', {}).get('enabled', False) and node.metadata.get('kvm', {}).get('gui', False),
-    },
-}
+    }
+
+if node.metadata.get('kvm', {}).get('enabled', False):
+    svc_systemd['libvirt-bin'] = {
+        'running': True,
+        'enabled': True,
+        'needs': ['pkg_apt:libvirt-bin'],
+    }
+else:
+    svc_systemd['libvirt-bin'] = {
+        'running': False,
+        'enabled': False,
+    }
