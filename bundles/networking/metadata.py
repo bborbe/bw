588ab2268f946c0a586905c11997024833cb4e60
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-17 10:48:50 +0100

    enable routing

--- bundles/networking/metadata.py
+++ bundles/networking/metadata.py
@@ -1,3 +1,10 @@
 def defaultnameservers(metadata):
     metadata.setdefault('networking', {}).setdefault('nameservers', ['8.8.4.4', '8.8.8.8'])
     return metadata
+
+
+def enable_routing(metadata):
+    if metadata.get('networking', {}).get('enabled', False):
+        options = metadata.setdefault('sysctl', {}).setdefault('options', {})
+        options['net.ipv4.ip_forward'] = '1'
+    return metadata
