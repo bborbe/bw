3b3dc32ad63d0d2e2452d3f6f8be24769d5845e9
Benjamin Borbe <bborbe@nova.fritz.box>
2017-01-22 17:57:54 +0100

    remove atop

--- bundles/debug/items.py
+++ bundles/debug/items.py
@@ -1,10 +1,13 @@
 if not (node.os == 'ubuntu' and node.os_version == (16, 4) or node.os == 'debian' and node.os_version == (8, 0)):
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
-pkg_apt = {}
+pkg_apt = {
+    'atop': {
+        'installed': False,
+    },
+}
 
 debugs_pkgs = (
-    'atop',
     'bmon',
     'bonnie++',
     'dnsutils',
