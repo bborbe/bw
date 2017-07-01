daad7ec5ee32b462898ebcf3f61f199ae98a3780
Benjamin Borbe <bborbe@rocketnews.de>
2017-07-01 12:46:19 +0200

    improve route to k8s networks

--- nodes/hm.fire.py
+++ nodes/hm.fire.py
@@ -62,8 +62,7 @@ nodes['hm.fire'] = {
             },
             'routes': {
                 'up route add -net 172.16.0.0/12 gw 192.168.178.2': {},
-                # 'up route add -net 172.16.72.0/24 gw 192.168.178.2': {},
-                # 'up route add -net 172.16.80.0/24 gw 192.168.178.2': {},
+                'up route add -net 172.16.24.0/24 gw 192.168.178.5': {},
                 'up route add -net 192.168.2.0/24 gw 192.168.178.2': {},
             },
         },
--- nodes/hm.nuke.py
+++ nodes/hm.nuke.py
@@ -60,8 +60,7 @@ nodes['hm.nuke'] = {
             },
             'routes': {
                 'up route add -net 172.16.0.0/12 gw 192.168.178.2': {},
-                # 'up route add -net 172.16.72.0/24 gw 192.168.178.2': {},
-                # 'up route add -net 172.16.80.0/24 gw 192.168.178.2': {},
+                'up route add -net 172.16.22.0/24 gw 192.168.178.3': {},
                 'up route add -net 192.168.2.0/24 gw 192.168.178.2': {},
             },
         },
