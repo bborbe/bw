255782700f79ddb57d65a0e948e30e4c1f061e3a
Benjamin Borbe <bborbe@rocketnews.de>
2017-12-21 23:39:46 +0100

    install package via apt bundle

---
+++ bundles/base/metadata.py
@@ -0,0 +1,39 @@
+@metadata_processor
+def install_mountall(metadata):
+    metadata.setdefault('apt', {}).setdefault('packages', {}).setdefault('mountall', {}).setdefault('installed', node.os == 'ubuntu')
+    return metadata, DONE
+
+
+@metadata_processor
+def install_apt_packages(metadata):
+    pkgs_install = (
+        'bash',
+        'curl',
+        'wget',
+        'screen',
+        'mailutils',
+        'postfix',
+        'augeas-tools',
+        'tmux',
+        'vim',
+        'net-tools',
+        'iproute2',
+        'ca-certificates',
+        'iputils-ping',
+        'openssh-client',
+        'rsync',
+        'nfs-common',
+    )
+    for package_name in pkgs_install:
+        metadata.setdefault('apt', {}).setdefault('packages', {}).setdefault(package_name, {}).setdefault('installed', True)
+    return metadata, DONE
+
+
+@metadata_processor
+def uninstall_apt_packages(metadata):
+    pkgs_uninstall = (
+        'mlocate',
+    )
+    for package_name in pkgs_uninstall:
+        metadata.setdefault('apt', {}).setdefault('packages', {}).setdefault(package_name, {}).setdefault('installed', False)
+    return metadata, DONE
