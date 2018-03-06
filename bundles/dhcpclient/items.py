b3532700eae99a818b9209789327a2bf19ee14d7
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-06 20:19:11 +0100

    always remove dhcpcd5 to fix raspbian get no second ip

--- bundles/dhcpclient/items.py
+++ bundles/dhcpclient/items.py
@@ -1,13 +1,19 @@
 if node.os != 'ubuntu' and node.os != 'debian':
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
-files = {
-    '/etc/dhcp/dhclient.conf': {
-        'source': 'dhclient.conf',
-        'content_type': 'text',
-        'mode': '0644',
-        'owner': 'root',
-        'group': 'root',
-        'context': {},
-    },
+pkg_apt = {}
+
+files = {}
+
+files['/etc/dhcp/dhclient.conf'] = {
+    'source': 'dhclient.conf',
+    'content_type': 'text',
+    'mode': '0644',
+    'owner': 'root',
+    'group': 'root',
+    'context': {},
+}
+
+pkg_apt['dhcpcd5'] = {
+    'installed': False,
 }
