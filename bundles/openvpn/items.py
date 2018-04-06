b99ba9b31cc45b6ebfe0ba30317b36b42532009a
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-06 16:23:04 +0200

    fix openvpn start on reboot

--- bundles/openvpn/items.py
+++ bundles/openvpn/items.py
@@ -2,28 +2,33 @@ if node.os != 'ubuntu' and node.os != 'debian':
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
 files = {}
+pkg_apt = {}
+svc_systemd = {}
 
-pkg_apt = {
-    'openvpn': {
-        'installed': node.metadata.get('openvpn', {}).get('enabled', False),
-    },
-    'easy-rsa': {
-        'installed': node.metadata.get('openvpn', {}).get('enabled', False),
-    },
+pkg_apt['openvpn'] = {
+    'installed': node.metadata.get('openvpn', {}).get('enabled', False),
+}
+pkg_apt['easy-rsa'] = {
+    'installed': node.metadata.get('openvpn', {}).get('enabled', False),
 }
 
-svc_systemd = {}
-if node.os == 'ubuntu':
-    svc_systemd['openvpn'] = {
-        'running': node.metadata.get('openvpn', {}).get('enabled', False),
-        'enabled': node.metadata.get('openvpn', {}).get('enabled', False),
-        'needs': ['pkg_apt:openvpn'],
-    }
+svc_systemd['openvpn'] = {
+    'running': node.metadata.get('openvpn', {}).get('enabled', False),
+    'enabled': node.metadata.get('openvpn', {}).get('enabled', False),
+    'needs': ['pkg_apt:openvpn'],
+}
 
-if node.os == 'debian':
-    for service, data in node.metadata.get('openvpn', {}).get('services', {}).items():
-        svc_systemd['openvpn@{}.service'.format(service)] = {
-            'running': node.metadata.get('openvpn', {}).get('enabled', False) and data.get('enabled', False),
-            'enabled': node.metadata.get('openvpn', {}).get('enabled', False) and data.get('enabled', False),
-            'needs': ['pkg_apt:openvpn'],
-        }
+files['/etc/default/openvpn'] = {
+    'source': 'openvpn',
+    'content_type': 'mako',
+    'mode': '0644',
+    'owner': 'root',
+    'group': 'root',
+    'context': {
+        'services': node.metadata.get('openvpn', {}).get('services', {}).keys(),
+    },
+    'triggers': [
+        'svc_systemd:openvpn:restart',
+        'action:systemd-reload'
+    ],
+}
