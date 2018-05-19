fb835e93bbc6558b640fab1a9c33f144695cb9e0
Benjamin Borbe <bborbe@rocketnews.de>
2018-05-19 16:29:44 +0200

    improve openvpn

--- bundles/openvpn/items.py
+++ bundles/openvpn/items.py
@@ -12,11 +12,17 @@ pkg_apt['easy-rsa'] = {
     'installed': node.metadata.get('openvpn', {}).get('enabled', False),
 }
 
-svc_systemd['openvpn'] = {
-    'running': node.metadata.get('openvpn', {}).get('enabled', False),
-    'enabled': node.metadata.get('openvpn', {}).get('enabled', False),
-    'needs': ['pkg_apt:openvpn'],
-}
+if node.metadata.get('openvpn', {}).get('enabled', False):
+    svc_systemd['openvpn'] = {
+        'running': True,
+        'enabled': True,
+        'needs': ['pkg_apt:openvpn'],
+    }
+else:
+    svc_systemd['openvpn'] = {
+        'running': False,
+        'enabled': False,
+    }
 
 for service, data in node.metadata.get('openvpn', {}).get('services', {}).items():
     svc_systemd['openvpn@{}.service'.format(service)] = {
@@ -25,17 +31,22 @@ for service, data in node.metadata.get('openvpn', {}).get('services', {}).items(
         'needs': ['pkg_apt:openvpn'],
     }
 
-files['/etc/default/openvpn'] = {
-    'source': 'openvpn',
-    'content_type': 'mako',
-    'mode': '0644',
-    'owner': 'root',
-    'group': 'root',
-    'context': {
-        'services': node.metadata.get('openvpn', {}).get('services', {}).keys(),
-    },
-    'triggers': [
-        'svc_systemd:openvpn:restart',
-        'action:systemd-reload'
-    ],
-}
+if node.metadata.get('openvpn', {}).get('enabled', False):
+    files['/etc/default/openvpn'] = {
+        'source': 'openvpn',
+        'content_type': 'mako',
+        'mode': '0644',
+        'owner': 'root',
+        'group': 'root',
+        'context': {
+            'services': node.metadata.get('openvpn', {}).get('services', {}).keys(),
+        },
+        'triggers': [
+            'svc_systemd:openvpn:restart',
+            'action:systemd-reload'
+        ],
+    }
+else:
+    files['/etc/default/openvpn'] = {
+        'delete': True,
+    }
