dfce8ca096f4e0712f8bbd2597dccf2b35361578
Benjamin Borbe <bborbe@rocketnews.de>
2019-01-30 11:21:48 +0100

    add noirq

--- bundles/grub/metadata.py
+++ bundles/grub/metadata.py
@@ -1,6 +1,7 @@
 @metadata_processor
 def default_cmd_args(metadata):
     metadata.setdefault('grub', {}).setdefault('cmd_args', {}).setdefault('net.ifnames=0', {})
+    metadata.setdefault('grub', {}).setdefault('cmd_args', {}).setdefault('acpi=noirq', {})
     if metadata.get('grub', {}).get('serial', False):
         metadata.setdefault('grub', {}).setdefault('cmd_args', {}).setdefault('console=tty0', {})
         metadata.setdefault('grub', {}).setdefault('cmd_args', {}).setdefault('console=ttyS0,115200n8', {})
