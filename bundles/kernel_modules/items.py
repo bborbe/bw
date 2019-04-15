35510c92c399f8b9d036ca42a82567268aff7fd3
Benjamin Borbe <bborbe@rocketnews.de>
2019-04-15 10:33:55 +0200

    manage /etc/modprobe.d/blacklist.conf

--- bundles/kernel_modules/items.py
+++ bundles/kernel_modules/items.py
@@ -1,15 +1,23 @@
 if node.os != 'ubuntu' and node.os != 'raspbian':
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
-files = {
-    '/etc/modules': {
-        'source': 'modules',
-        'content_type': 'mako',
-        'context': {
-            'modules': node.metadata.get('kernel_modules', {}),
-        },
-        'owner': 'root',
-        'group': 'root',
-        'mode': '0644',
+files = {}
+
+files['/etc/modules'] = {
+    'source': 'modules',
+    'content_type': 'mako',
+    'context': {
+        'modules': node.metadata.get('kernel_modules', {}),
     },
+    'owner': 'root',
+    'group': 'root',
+    'mode': '0644',
+}
+
+files['/etc/modprobe.d/blacklist.conf'] = {
+    'source': 'blacklist.conf',
+    'content_type': 'text',
+    'owner': 'root',
+    'group': 'root',
+    'mode': '0644',
 }
