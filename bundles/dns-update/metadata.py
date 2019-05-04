8cd2ec7c35d65c6c93e3f9d3c6461d8ed539df0f
Benjamin Borbe <bborbe@rocketnews.de>
2019-05-04 14:55:36 +0200

    move teamvault access to bundle

--- bundles/dns-update/metadata.py
+++ bundles/dns-update/metadata.py
@@ -1,8 +1,14 @@
+import bwtv as teamvault
+
 @metadata_processor
 def crons(metadata):
     metadata.setdefault('cron', {}).setdefault('jobs', {})
     dns_update = metadata.get('dns-update', {})
     for name, data in dns_update.get('updates', {}).items():
+        if 'private_hash' in data:
+            data['private'] = teamvault.file(data['private_hash'], site='benjamin-borbe')
+        if 'key_hash' in data:
+            data['key'] = teamvault.file(data['key_hash'], site='benjamin-borbe')
         metadata['cron']['jobs']['dns-update'] = {
             'enabled': dns_update.get('enabled', False),
             'expression': '/usr/local/bin/dns-update.sh {server} /etc/dns-update/keys/{name} {zone} {node} {ip} >> /var/log/dns-update.log'.format(
