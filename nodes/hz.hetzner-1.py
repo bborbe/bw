f90047242ff8eadcf49286fb86c6fcf966ecf6d1
Benjamin Borbe <bborbe@rocketnews.de>
2020-03-30 21:46:44 +0200

    add hetzner node

---
+++ nodes/hz.hetzner-1.py
@@ -0,0 +1,35 @@
+nodes['hz.hetzner-1'] = {
+    'hostname': 'hetzner-1.benjamin-borbe.de',
+    'metadata': {
+        'os': 'ubuntu',
+        'release': 'bionic',
+        'iptables': {
+            'enabled': True,
+            'nat_interfaces': [],
+            'rules': {
+                'filter': {
+                    # allow forward
+                    '-A FORWARD -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 25 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 53 -j ACCEPT',
+                    '-A INPUT -p udp -m state --state NEW -m udp --dport 53 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 143 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 465 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 587 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 993 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 2222 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 3128 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 6443 -j ACCEPT',
+                    '-A INPUT -p tcp -m state --state NEW -m tcp --dport 64738 -j ACCEPT',
+                },
+            },
+        },
+        'users': {
+            'bborbe': {
+                'enabled': True,
+            },
+        },
+    },
+}
