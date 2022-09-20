60651b4f26e9c1b717323df7f3fc46a330bd69d0
Benjamin Borbe <bborbe@rocketnews.de>
2022-09-20 19:59:12 +0200

    add new ssh-key

--- bundles/backup_client/metadata.py
+++ bundles/backup_client/metadata.py
@@ -4,7 +4,8 @@ def add_backup_key(metadata):
         'users': {
             'root': {
                 'authorized_keys': {
-                    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM70gDI1cuIKu15VAEYnlWMFi3zGPf6shtQCzrHBv1nOOkfPdXlXACC4H5+MiTV5foAIA8PUaqOV9gow1w639TnWDL2DwPJ5RsT+P5g4eWszW5xQPo0zAKuvlTMB9JkDXGx1OpOE4e9n3++71yuvF/wVlqYxJwxeWXCdHf2ayx6OrTMcSMUIi+zqD494YBhKt+QJAiRrXGNU82FczJeK3iRMTtd+LUeGtnEoskcDOwhGfOXGsUGt3BMWLiDhGXp4ZvKUhNSTz5Kr9OCQT/uWam3nXciZrx1a2kVFJhd1ur81LRqxxDMGQjS29z6Vpd2vxG/P8mP3w2r7fvoqE33hpv': {}
+                    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM70gDI1cuIKu15VAEYnlWMFi3zGPf6shtQCzrHBv1nOOkfPdXlXACC4H5+MiTV5foAIA8PUaqOV9gow1w639TnWDL2DwPJ5RsT+P5g4eWszW5xQPo0zAKuvlTMB9JkDXGx1OpOE4e9n3++71yuvF/wVlqYxJwxeWXCdHf2ayx6OrTMcSMUIi+zqD494YBhKt+QJAiRrXGNU82FczJeK3iRMTtd+LUeGtnEoskcDOwhGfOXGsUGt3BMWLiDhGXp4ZvKUhNSTz5Kr9OCQT/uWam3nXciZrx1a2kVFJhd1ur81LRqxxDMGQjS29z6Vpd2vxG/P8mP3w2r7fvoqE33hpv': {},
+                    'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9Vu6RMdaevopPkAmH3gx41HhTA+0IExPMn+4OKrKrG benjamin.borbe@gmail.com': {},
                 }
             }
         }
--- groups/meta/user.py
+++ groups/meta/user.py
@@ -17,6 +17,7 @@ groups['meta-user'] = {
                 'sudo': True,
                 'authorized_keys': {
                     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCOw/yh7+j3ygZp2aZRdZDWUh0Dkj5N9/USdiLSoS+0CHJta+mtSxxmI/yv1nOk7xnuA6qtjpxdMlWn5obtC9xyS6T++tlTK9gaPwU7a/PObtoZdfQ7znAJDpX0IPI06/OH1tFE9kEutHQPzhCwRaIQ402BHIrUMWzzP7Ige8Oa0HwXH4sHUG5h/V/svzi9T0CKJjF8dTx4iUfKX959hT8wQnKYPULewkNBFv6pNfWIr8EzvIEQcPmmm3tP+dQPKg5QKVi6jPdRla+t5HXfhXu0W3WCDa2s0VGmJjBdMMowr5MLNYI79MKziSV1w1IWL17Z58Lop0zEHqP7Ba0Aooqd': {},
+                    'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9Vu6RMdaevopPkAmH3gx41HhTA+0IExPMn+4OKrKrG benjamin.borbe@gmail.com': {},
                 },
                 'full_name': 'Benjamin Borbe',
                 'dotfiles': 'https://github.com/bborbe/dotfiles.git',
