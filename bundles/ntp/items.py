bb3c051329b931aa54ea88d36b45c273ffbd6c78
Benjamin Borbe <bborbe@rocketnews.de>
2022-09-20 19:58:38 +0200

    remove ntp

--- bundles/ntp/items.py
+++
@@ -1,6 +0,0 @@
-if node.os != 'ubuntu' and node.os != 'raspbian':
-    raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
-
-pkg_apt = {
-    'ntpdate': {},
-}
