524db2d315103ee6209af23f6e8162a10650ca5f
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-01-05 21:28:36 +0100

    add docker engine

---
+++ bundles/docker-engine/metadata.py
@@ -0,0 +1,16 @@
+@metadata_reactor
+def docker_repo(metadata):
+    if metadata.get('docker', {}).get('enabled', False):
+        return {
+            'apt': {
+                'repos': {
+                    'docker': {
+                        'installed': True,
+                        'gpg_key': '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+                        'sources': ['deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable'],
+                    },
+                }
+            }
+        }
+    else:
+        return {}
