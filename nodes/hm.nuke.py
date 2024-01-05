e6722399ed3a6b4bdf640f534a1f3ec66263b689
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-01-05 10:14:44 +0100

    add hm.nuke

---
+++ nodes/hm.nuke.py
@@ -0,0 +1,45 @@
+nodes['hm.nuke'] = {
+    'hostname': 'nuke.hm.benjamin-borbe.de',
+    'groups': {
+        'ubuntu-jammy',
+    },
+    'metadata': {
+        'groups': {
+            'data': {
+                'enabled': True,
+            },
+        },
+        'iptables': {
+            'enabled': True,
+            'nat_interfaces': [],
+            'rules': {
+                'filter': {
+                    # allow forward
+                    '-A FORWARD -j ACCEPT',
+                    '-A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT',
+                    '-A INPUT -m state --state NEW -p tcp --dport 6443 -j ACCEPT',
+                },
+            },
+        },
+        'networking': {
+            'interfaces': {
+                'eth0': {
+                    'address': '192.168.178.5',
+                    'netmask': '255.255.255.0',
+                    'gateway': '192.168.178.1',
+                },
+            },
+            'routes': {},
+        },
+        'users': {
+            'bborbe': {
+                'enabled': True,
+                'groups': ['data', 'sudo'],
+            },
+            'jana': {
+                'enabled': True,
+                'groups': ['data'],
+            },
+        },
+    },
+}
