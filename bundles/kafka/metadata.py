d1c850117c87fad4220c8ffba596ce039b1988cc
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-01-26 18:08:07 +0100

    fix name

--- bundles/kafka/metadata.py
+++ bundles/kafka/metadata.py
@@ -1,7 +1,7 @@
 @metadata_reactor
 def iptables(metadata):
     rules = set()
-    if metadata.get('trading', {}).get('enabled', False):
+    if metadata.get('kafka', {}).get('enabled', False):
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 9092 -j ACCEPT')
     return {
         'iptables': {
