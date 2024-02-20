42f1067e3e24dd70b133dfee7a44c94ec0f2b2bd
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-02-20 10:57:37 +0100

    install chrome

--- bundles/ubuntu-desktop/metadata.py
+++ bundles/ubuntu-desktop/metadata.py
@@ -6,6 +6,7 @@ def install_apt_packages(metadata):
         'gnome-terminal',
         'ubuntu-desktop',
         'unzip',
+        'google-chrome-stable',
     )
     result = {
         'apt': {
@@ -17,3 +18,22 @@ def install_apt_packages(metadata):
             'installed': metadata.get('ubuntu-desktop', {}).get('enabled', False)
         }
     return result
+
+
+@metadata_reactor
+def google_chrome_repo(metadata):
+    if metadata.get('ubuntu-desktop', {}).get('enabled', False):
+        return {
+            'apt': {
+                'repos': {
+                    'google-chrome': {
+                        'installed': True,
+                        'gpg_key': 'E88979FB9B30ACF2',
+                        'sources': ['deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'],
+                        #'sources': ['deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main'],
+                    },
+                }
+            }
+        }
+    else:
+        return {}
