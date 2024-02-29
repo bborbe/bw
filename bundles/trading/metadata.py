e9aa95bc604cd9553c72d4bfd0839420b51f59d5
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-02-29 12:41:05 +0100

    only if trading is enabled

--- bundles/trading/metadata.py
+++ bundles/trading/metadata.py
@@ -1,13 +1,18 @@
-@metadata_reactor
+@metadata_reactor.provides(
+    'iptables/rules/filter',
+)
 def iptables(metadata):
-    rules = set()
     if metadata.get('trading', {}).get('enabled', False):
+        rules = set()
+        # develop ports
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 20000:21000 -j ACCEPT')
+        # prod ports
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 30000:31000 -j ACCEPT')
-    return {
-        'iptables': {
-            'rules': {
-                'filter': rules,
+        return {
+            'iptables': {
+                'rules': {
+                    'filter': rules,
+                },
             },
-        },
-    }
+        }
+    return {}
