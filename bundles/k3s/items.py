14ead5e610bb85408b8b5d1c2bda3fe60e97efc7
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-03-03 09:33:08 +0100

    remove storage directories

--- bundles/k3s/items.py
+++ bundles/k3s/items.py
@@ -11,21 +11,6 @@ if node.metadata.get('k3s', {}).get('enabled', False):
         'owner': 'root',
         'group': 'root',
     }
-    directories['/var/lib/rancher/k3s/storage/hdd'] = {
-        'mode': '0700',
-        'owner': 'root',
-        'group': 'root',
-    }
-    directories['/var/lib/rancher/k3s/storage/ssd'] = {
-        'mode': '0700',
-        'owner': 'root',
-        'group': 'root',
-    }
-    directories['/var/lib/rancher/k3s/storage/lvm'] = {
-        'mode': '0700',
-        'owner': 'root',
-        'group': 'root',
-    }
     svc_systemd['k3s'] = {
         'running': True,
         'enabled': True,
