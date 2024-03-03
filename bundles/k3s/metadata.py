38d1245b32e74cd0ca0859bde57e9492bd29b7ec
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-03-03 09:33:25 +0100

    add k8s ports

--- bundles/k3s/metadata.py
+++ bundles/k3s/metadata.py
@@ -3,7 +3,11 @@ def iptables(metadata):
     rules = set()
     if metadata.get('k3s', {}).get('enabled', False):
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT')
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 2379 -j ACCEPT')
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 2380 -j ACCEPT')
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 6443 -j ACCEPT')
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 8472 -j ACCEPT')
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 10250 -j ACCEPT')
         rules.add('-A INPUT -m state --state NEW -p tcp --dport 30000:32767 -j ACCEPT')
     return {
         'iptables': {
