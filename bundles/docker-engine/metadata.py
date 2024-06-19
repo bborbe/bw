8d54bf9545a6320bdf691aaefb21020cdc56f41e
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-06-19 18:42:36 +0200

    use release in docker source

--- bundles/docker-engine/metadata.py
+++ bundles/docker-engine/metadata.py
@@ -7,7 +7,7 @@ def docker_repo(metadata):
                     'docker': {
                         'installed': True,
                         'gpg_key': '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-                        'sources': ['deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable'],
+                        'sources': ['deb [arch=amd64] https://download.docker.com/linux/ubuntu {} stable'.format(node.metadata.get('release'))],
                     },
                 }
             }
