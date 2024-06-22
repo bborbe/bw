273dee9150936de2f4e3d94d1eb3a4b900ba77ce
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-06-22 11:30:21 +0200

    revent uninstall

--- bundles/ubuntu-desktop/metadata.py
+++ bundles/ubuntu-desktop/metadata.py
@@ -2,41 +2,46 @@
     'apt/packages',
 )
 def install_apt_packages(metadata):
-    pkgs = (
-        'build-essential',
-        'curl',
-        'curl',
-        'gedit',
-        'gnome-terminal',
-        'libbz2-dev',
-        'libffi-dev',
-        'libldap2-dev',
-        'liblzma-dev',
-        'libncursesw5-dev',
-        'libreadline-dev',
-        'libsasl2-dev',
-        'libsqlite3-dev',
-        'libssl-dev',
-        'libxml2-dev',
-        'libxml2-utils',
-        'libxmlsec1-dev',
-        'python3-dev',
-        'python3-venv',
-        'rsync',
-        'tk-dev',
-        'ubuntu-desktop',
-        'unixodbc-dev',
-        'unzip',
-        'xz-utils',
-        'zlib1g-dev',
-    )
     result = {
         'apt': {
             'packages': {}
         }
     }
-    for package_name in pkgs:
+
+    for package_name in (
+        'gedit',
+        'gnome-terminal',
+        'tk-dev',
+        'ubuntu-desktop',
+    ):
         result['apt']['packages'][package_name] = {
             'installed': metadata.get('ubuntu-desktop', {}).get('enabled', False)
         }
+
+    if metadata.get('ubuntu-desktop', {}):
+        for package_name in (
+            'build-essential',
+            'libbz2-dev',
+            'libffi-dev',
+            'libldap2-dev',
+            'liblzma-dev',
+            'libncursesw5-dev',
+            'libreadline-dev',
+            'libsasl2-dev',
+            'libsqlite3-dev',
+            'libssl-dev',
+            'libxml2-dev',
+            'libxml2-utils',
+            'libxmlsec1-dev',
+            'python3-dev',
+            'python3-venv',
+            'rsync',
+            'unixodbc-dev',
+            'unzip',
+            'xz-utils',
+            'zlib1g-dev',
+        ):
+            result['apt']['packages'][package_name] = {
+                'installed': True,
+            }
     return result
