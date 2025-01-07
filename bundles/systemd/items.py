883efae20bffdb77385e6d3fc2e683e69222b00a
Benjamin Borbe <bborbe@rocketnews.de>
2025-01-07 15:37:58 +0000

    cleanup journal logs

--- bundles/systemd/items.py
+++ bundles/systemd/items.py
@@ -2,8 +2,16 @@ if node.os != 'ubuntu' and node.os != 'raspbian':
     raise Exception('{} {} is not supported by this bundle'.format(node.os, node.os_version))
 
 actions = {}
+files = {}
 
 actions['systemd-reload'] = {
     'command': 'systemctl daemon-reload',
     'triggered': True,
+}
+
+files['/etc/systemd/journald.conf'] = {
+    'source': 'journald.conf',
+    'mode': '0644',
+    'owner': 'root',
+    'group': 'root',
 }
