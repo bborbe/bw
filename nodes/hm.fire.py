afff4f7b3b362a7b01ea3b772403fb4d693f146b
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-03-08 11:05:42 +0100

    update golang

--- nodes/hm.fire.py
+++ nodes/hm.fire.py
@@ -25,7 +25,7 @@ nodes['hm.fire'] = {
         'golang': {
             'enabled': True,
             'arch': 'amd64',
-            'version': '1.24.0',
+            'version': '1.24.1',
             'os': 'linux',
         },
         'backup_client': {
