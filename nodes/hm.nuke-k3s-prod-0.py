94897d998ad9d43a97863681d32f2fd7cf9b3532
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-03-23 00:24:58 +0100

    add dev-0 and prod-0

---
+++ nodes/hm.nuke-k3s-prod-0.py
@@ -0,0 +1,62 @@
+nodes['hm.nuke-k3s-prod-0'] = {
+    'hostname': 'nuke-k3s-prod-0.hm.benjamin-borbe.de',
+    'groups': {
+        'ubuntu-jammy',
+    },
+    'metadata': {
+        'netplan': {
+            'enabled': True,
+            'ethernets': {
+                'eth0': {
+                    'dhcp4': False,
+                    'dhcp6': False,
+                    'optional': True,
+                    'match': {
+                        'name': 'en*',
+                    },
+                    'set-name': 'eth0',
+                    'addresses': ['192.168.178.28/24'],
+                    'routes': [
+                        {
+                            'to': 'default',
+                            'via': '192.168.178.1',
+                        }
+                    ],
+                    'nameservers': {
+                        'addresses': ['8.8.8.8', '8.8.4.4'],
+                        'search': ['hm.benjamin-borbe.de'],
+                    },
+                },
+            },
+        },
+        'kvm-guest': {
+            'enabled': True,
+        },
+        'backup_client': {
+            'enabled': True,
+        },
+        'k3s': {
+            'enabled': True,
+            'agent': True,
+        },
+        'iptables': {
+            'enabled': True,
+            'nat_interfaces': [],
+            'rules': {
+                'filter': {
+                    '-A INPUT -j ACCEPT',
+                    '-A FORWARD -j ACCEPT',
+                },
+            },
+        },
+        'users': {
+            'bborbe': {
+                'enabled': True,
+                'groups': ['sudo'],
+            },
+            'install': {
+                'enabled': False,
+            },
+        },
+    },
+}
