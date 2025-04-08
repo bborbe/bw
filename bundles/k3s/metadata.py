feda3d67d1edbfce08b86dbdf5e229ae661c7f91
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-04-08 18:21:25 +0200

    remove /root/scripts git checkout + change iptables rules

--- bundles/k3s/metadata.py
+++ bundles/k3s/metadata.py
@@ -4,13 +4,12 @@
 def iptables(metadata):
     rules = set()
     if metadata.get('k3s', {}).get('enabled', False):
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 2379 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 2380 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 6443 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 8472 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 10250 -j ACCEPT')
-        rules.add('-A INPUT -m state --state NEW -p tcp --dport 30000:32767 -j ACCEPT')
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 6443 -s {} -j ACCEPT'.format(metadata['k3s']['network']))
+        rules.add('-A INPUT -m state --state NEW -p udp --dport 8472 -s {} -j ACCEPT'.format(metadata['k3s']['network']))
+        rules.add('-A INPUT -m state --state NEW -p tcp --dport 10250 -s {} -j ACCEPT'.format(metadata['k3s']['network']))
+        if not metadata.get('k3s', {}).get('agent', False):
+            rules.add('-A INPUT -m state --state NEW -p tcp --dport 2379 -s {} -j ACCEPT'.format(metadata['k3s']['network']))
+            rules.add('-A INPUT -m state --state NEW -p tcp --dport 2380 -s {} -j ACCEPT'.format(metadata['k3s']['network']))
     return {
         'iptables': {
             'rules': {
@@ -18,21 +17,3 @@ def iptables(metadata):
             },
         },
     }
-
-
-@metadata_reactor.provides(
-    'git/clones/repo',
-)
-def git_clone_scripts(metadata):
-    if metadata.get('k3s', {}).get('enabled', False):
-        return {
-            'git': {
-                'clones': {
-                    'scripts': {
-                        'repo': 'https://github.com/bborbe/scripts.git',
-                        'target': '/root/scripts',
-                    },
-                },
-            },
-        }
-    return {}
