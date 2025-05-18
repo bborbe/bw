d64479e04c3ce591e5d8ee09b486244a4ebabbca
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-05-18 15:14:14 +0200

    remove config if not defined

--- bundles/udev/items.py
+++ bundles/udev/items.py
@@ -3,13 +3,18 @@ if node.os != 'ubuntu' and node.os != 'raspbian':
 
 files = {}
 
-files['/etc/udev/rules.d/10-network.rules'] = {
-    'source': '10-network.rules',
-    'content_type': 'mako',
-    'context': {
-        'modules': node.metadata.get('udev', {}),
-    },
-    'owner': 'root',
-    'group': 'root',
-    'mode': '0644',
-}
+if len(node.metadata.get('udev', {})) > 0:
+    files['/etc/udev/rules.d/10-network.rules'] = {
+        'source': '10-network.rules',
+        'content_type': 'mako',
+        'context': {
+            'modules': node.metadata.get('udev', {}),
+        },
+        'owner': 'root',
+        'group': 'root',
+        'mode': '0644',
+    }
+else:
+    files['/etc/udev/rules.d/10-network.rules'] = {
+        'delete': True,
+    }
