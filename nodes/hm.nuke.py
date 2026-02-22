f7379df0094490a0233c3d87e80e6d584ba595b8
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-02-22 18:53:12 +0100

    rm test alert

--- nodes/hm.nuke.py
+++ nodes/hm.nuke.py
@@ -29,9 +29,6 @@ nodes['hm.nuke'] = {
                 'devices': ['/dev/nvme0n1', '/dev/nvme1n1', '/dev/nvme2n1', '/dev/nvme3n1'],
             }
         },
-        'monit': {
-            'test_alert': True,
-        },
         'msmtp': {
             'enabled': True,
             'host': '172.16.90.1',
