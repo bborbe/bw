1df12fc16db3ec5a1b76a7b692fea9aa34a43411
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-03-10 13:41:57 +0100

    update golang

--- bundles/golang/items.py
+++ bundles/golang/items.py
@@ -2,7 +2,7 @@ directories = {}
 actions = {}
 files = {}
 
-default_golang_version = '1.26.0'
+default_golang_version = '1.26.1'
 
 if node.metadata.get('golang', {}).get('enabled', False):
     directories['/opt/go'] = {
