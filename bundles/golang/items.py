3908941f4364416852bab27b6fea913049d37979
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-08-19 22:20:21 +0200

    bump default_golang_version to 1.27.0

--- bundles/golang/items.py
+++ bundles/golang/items.py
@@ -2,7 +2,7 @@ directories = {}
 actions = {}
 files = {}
 
-default_golang_version = '1.26.6'
+default_golang_version = '1.27.0'
 
 if node.metadata.get('golang', {}).get('enabled', False):
     directories['/opt/go'] = {
